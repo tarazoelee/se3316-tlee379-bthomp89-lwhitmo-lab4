a845ef1c8991a4ab2a7f0e36aa2dde4c9db27c2c
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-11-22 12:55:56 -0500

    change how to set up firebase init

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const app = firebase.initializeApp({
-    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDAyEuEezG0UcCsPmTSTbRsczQkvL6LKBc",
+  authDomain: "lab4-4c879.firebaseapp.com",
+  projectId: "lab4-4c879",
+  storageBucket: "lab4-4c879.appspot.com",
+  messagingSenderId: "382355225981",
+  appId: "1:382355225981:web:7816edf99ab82c1526e014",
+  measurementId: "G-8WBYCGVXEE"
 })
 
 export const auth = app.auth();
