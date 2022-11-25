66359c2684c6a6dc9d4675ed27e8a13baf350370
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-11-25 08:51:53 -0500

    playlist styling

--- Client/src/components/Playlist/Playlist.js
+++ Client/src/components/Playlist/Playlist.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Playlist.css'
+import SearchTracks from '../SearchTracks/SearchTracks';
 
+//page for each individual playlist
 function Playlist() {
   return (
     <div className='play-container'>
-        <p>Playlist Page</p>
+        <span>
+        <div className='addSongs'>
+            <h1>Add Songs</h1>
+        <p><SearchTracks></SearchTracks></p>
+        </div>
+        
+        <div className='playlistInfo'>
+            <h1>Playlist information!</h1>
+            </div>
+        </span>
     </div>
   )
 }
