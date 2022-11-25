5bc307ffb53bf395febcd0c9414db6075018cc99
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-11-25 12:35:04 -0500

    create Playlist button opening new field

--- Client/src/components/PlaylistList/PlaylistList.js
+++ Client/src/components/PlaylistList/PlaylistList.js
@@ -1,10 +1,20 @@
 import './PlaylistList.css'
 import {React, useState, useEffect} from 'react';
 import { useNavigate } from "react-router-dom";
+import Popup from '../Popup/popup';
+
 
 function PlaylistList() {
   const [items, setItems] = useState([])
   const [DataisLoaded, setLoading]= useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+ 
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+  const togglePopupClose=()=>{
+    setIsOpen(false);
+  }
   const history = useNavigate();
 
   //calls fetch data once when page loaded
@@ -31,11 +41,25 @@ function PlaylistList() {
   function openPlaylist(passed){
     history('/playlist/'+passed) //open new page for that playlist
   }
+  
 
   return (
     <div className='playlist-container'>
       <h3 className='title'>Browse playlists</h3>
-      <button class="btn btn-outline-light">Create New Playlist</button>
+      <div>
+    <input
+      type="button"
+      value="Create Playlist"
+      onClick={togglePopup}
+    />
+    {isOpen && <Popup
+      content={<>
+      <p></p>
+      <span>Name:  <input></input></span>
+      </>}
+      handleClose={togglePopup}
+    />}
+  </div>
       <div className='playlist-container' >{
       items.map((item) => ( 
         <div key = { item.id } className='track-container' onClick={()=>openPlaylist(item.id)}>
