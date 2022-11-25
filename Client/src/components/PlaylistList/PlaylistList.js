6cba75cf6fe821cecd85c55cc3c478c2bd224f54
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-11-25 10:33:54 -0500

    open playlist when clicked

--- Client/src/components/PlaylistList/PlaylistList.js
+++ Client/src/components/PlaylistList/PlaylistList.js
@@ -28,17 +28,17 @@ function PlaylistList() {
       return <div> <h1 className='text-white fs-5'> loading.... </h1> </div> ;
    }
 
-  function openPlaylist(){
-    history('/playlist/1') //open new page for that playlist
+  function openPlaylist(passed){
+    history('/playlist/'+passed) //open new page for that playlist
   }
 
   return (
     <div className='playlist-container'>
       <h3 className='title'>Browse playlists</h3>
       <button class="btn btn-outline-light">Create New Playlist</button>
-      <div className='playlist-container'  onClick={openPlaylist}>{
+      <div className='playlist-container' >{
       items.map((item) => ( 
-        <div key = { item.id } className='track-container' >
+        <div key = { item.id } className='track-container' onClick={()=>openPlaylist(item.id)}>
         <p className='text-black fs-5'>Name: {item.Name}</p>
        </div>
                     ))
