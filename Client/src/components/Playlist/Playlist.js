00485d75444ec29dc3d038f5446dbdf183d78ba9
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-11-25 13:48:09 -0500

    adding tracks to playlists front end

--- Client/src/components/Playlist/Playlist.js
+++ Client/src/components/Playlist/Playlist.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Playlist.css'
-import SearchTracks from '../SearchTracks/SearchTracks';
+import SearchTracksForPlaylist from '../SearchTracksForPlaylists/SearchTracksForPlaylist';
 import PlaylistInformation from '../Playlist Information/PlaylistInformation';
 import { Link, useNavigate } from "react-router-dom";
 
@@ -13,7 +13,7 @@ function Playlist() {
     <div className='dash-container'>
         <div className='searchtracks-container'>
         <h2 className="text-center mb-4">Add Songs</h2>
-            <SearchTracks></SearchTracks>
+            <SearchTracksForPlaylist></SearchTracksForPlaylist>
         </div>
         <div className='list-container'>
             <PlaylistInformation></PlaylistInformation>
