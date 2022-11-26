d883b24d08b8ee3086076131e933d4b78af15755
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-11-26 10:00:51 -0500

    back end to display tracks on a playlist

--- Client/src/components/Playlist Information/PlaylistInformation.js
+++ Client/src/components/Playlist Information/PlaylistInformation.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import './PlaylistInformation.css'
 
 function PlaylistInformation() {
+    const [items, setItems] = useState([])
+    const [DataisLoaded, setLoading]= useState(false)
+    function fetchData(){
+        fetch("/api/playlist")
+            .then((res) => res.json())
+            .then((json) => {
+                    setItems(json);
+                    setLoading(true);
+            ;
+        })
+    }
+    function fetchTracks(){
+        fetch("/api/tracks")
+            .then((res) => res.json())
+            .then((json) => {
+                    setItems(json);
+                    setLoading(true);
+            ;
+        })
+    }
+    useEffect(() => {
+        fetchData();
+        fetchTracks();
+    }, []);
   return (
     <div className='playlist-container'>
       <h3 className="text-center mb-4">Information</h3>
+      {items.map((item) => ( 
+            <div key = { item.id } className='track-container' >
+               <p>{ item.trackTitle }, Album: { item.albumTitle }, Artist: { item.artistName } </p>
+            </div>
+      ))}
     </div>
   )
 }
--- Server/Routes/Tracks.js
+++ Server/Routes/Tracks.js
@@ -5,5 +5,37 @@ const { db } = require("../../util/admin");
 const {tracks} = require('../handler/tracks');
 
 router.get('/', tracks)
+//get track info
+router.get('/:id', (req, res)=>{
+    const id = req.params.id;
+    lookForTracks(id).then((data)=>{
+        res.send(data)
+    })
+    .catch((err)=>{
+        res.send(err)
+    })
+})
 
+async function lookForTracks(id){
+    const citiesRef = db.collection('Tracks');
+    const snapshot = await citiesRef.where('trackId', '==', Number(id)).limit(1).get();
+    if (snapshot.empty) {
+    console.log('No matching documents.');
+    return;
+    }  
+    snapshot.forEach(doc => {
+    console.log(doc.id, '=>', doc.data());
+    sending ={
+        "trackId": doc.data().trackId,
+        "trackTitle": doc.data().trackTitle,
+        "artistName": doc.data().artistName,
+        "albumTitle": doc.data().albumTitle
+    }
+    })
+    console.log("break")
+    console.log(sending)
+    return sending
+    
+               
+}
 module.exports = router;
