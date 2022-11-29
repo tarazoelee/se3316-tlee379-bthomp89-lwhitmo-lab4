101f76a60bb8f87f3ba484d2736aad519713072d
Brayden Thompson <bthomp89@uwo.ca>
2022-11-29 01:00:43 -0500

    implemented all backend methods for admin

--- Server/Routes/Users.js
+++ Server/Routes/Users.js
@@ -7,36 +7,113 @@ const { users } = require("../handler/users");
 //Get all users from database
 router.get("/", users);
 
-//Add user to database
+//Add user to database (takes uid in url and email in body, creates document with docid = uid and sets email, isAdmin and disabled)
 router.post("/adduser/:id", (req, res) => {
   const uid = req.params.id;
-  const email = "jstest@gmail.com"; //needs to be in the req.body
-  const isAdmin = false;
-  const disabled = false;
-
-  addUser(uid, email, isAdmin, disabled);
-  res.send("Added User");
+  const email = "brayden@gmail.com"; //needs to be in the req.body
+  try {
+    addUser(uid, email);
+    return res.status(200).send("Added User " + uid);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
 });
 
-//Get a specfific user given email
-
 //Get a specific user given uid
+router.get("/getuser/:id", (req, res) => {
+  const uid = req.params.id;
+  try {
+    getUser(uid).then((data) => {
+      return res.status(200).send(data);
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
 
-//Update isAdmin flag for a specific user
+//Give a specific user admin privliges
+router.post("/giveadmin/:id", (req, res) => {
+  const uid = req.params.id;
+  giveAdmin(uid);
+  res.send("Gave " + uid + " Admin Privliges");
+});
 
-//Update disabled flag for a given user
+//Remove a specific users admin privliges
+router.post("/removeadmin/:id", (req, res) => {
+  const uid = req.params.id;
+  removeAdmin(uid);
+  res.send("Removed " + uid + " Admin Privliges");
+});
 
-async function addUser(uid, email, isAdmin, disabled) {
+//Set a users account to disabled
+router.post("/setdisabled/:id", (req, res) => {
+  const uid = req.params.id;
+  setDisabled(uid);
+  res.send("Account" + uid + "is now disabled");
+});
+
+//Undisable a users account
+router.post("/removedisabled/:id", (req, res) => {
+  const uid = req.params.id;
+  removeDisabled(uid);
+  res.send("Account" + uid + "is no longer disabled");
+});
+
+//fucntions to preform all route actions
+async function giveAdmin(uid) {
   const FieldValue = require("firebase-admin").firestore.FieldValue;
 
-  const res = await db.collection("Users").add({
-    uid: uid,
-    email: email,
-    isAdmin: isAdmin,
-    disabled: disabled,
+  const res = await db.collection("Users").doc(uid).update({
+    isAdmin: true,
   });
-
-  console.log("Added User: " + email + " to Users");
 }
 
+async function removeAdmin(uid) {
+  const FieldValue = require("firebase-admin").firestore.FieldValue;
+
+  const res = await db.collection("Users").doc(uid).update({
+    isAdmin: false,
+  });
+}
+
+async function setDisabled(uid) {
+  const FieldValue = require("firebase-admin").firestore.FieldValue;
+
+  const res = await db.collection("Users").doc(uid).update({
+    disabled: true,
+  });
+}
+
+async function removeDisabled(uid) {
+  const FieldValue = require("firebase-admin").firestore.FieldValue;
+
+  const res = await db.collection("Users").doc(uid).update({
+    disabled: false,
+  });
+}
+
+async function getUser(uid) {
+  const document = db.collection("Users").doc(uid);
+  let user = await document.get();
+  let response = user.data();
+  if (!user.exists) {
+    console.log("User doesn't exist");
+  } else {
+    console.log(response);
+  }
+  return response;
+}
+
+async function addUser(uid, email) {
+  await db
+    .collection("Users")
+    .doc("/" + uid + "/")
+    .create({
+      email: email,
+      isAdmin: false,
+      disabled: false,
+    });
+}
 module.exports = router;
