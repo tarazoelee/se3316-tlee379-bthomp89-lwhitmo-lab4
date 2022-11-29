d49e38499fbef0c06f9b5bfc8f20ddea23dedddd
Tara <tlee379@uwo.ca>
2022-11-29 12:02:39 -0500

    expanding songs on click in public playlists

--- Client/src/components/UnAuthPlaylist/UnAuthPlaylistVie.js
+++ Client/src/components/UnAuthPlaylist/UnAuthPlaylistVie.js
@@ -113,6 +113,37 @@ function UnAuthPlaylist() {
         })
     }, [items.length]);
 
+     //When track is clicked display additional info 
+    function getClicked(id, album, time){
+        const track = document.getElementById(id);
+
+        //only create additional children if they don't exist
+        if(!track.children[1]){
+            const div = document.createElement('div');
+            const info = document.createTextNode("Album: " + album + ' Length: ' + time)
+            const close = document.createElement('button');
+            
+            div.className='clicked-info';
+            close.className='close-btn'
+            
+            close.appendChild(
+                document.createTextNode('close')
+            )
+            div.appendChild(info);
+            div.appendChild(close);
+            track.appendChild(div);
+            close.addEventListener("click",()=>removeClicked(id));
+        }
+    console.log('clicked')
+    }
+
+    function removeClicked(id){
+        const track = document.getElementById(id);
+        while(track.children[1]){
+            track.removeChild(track.children[1])
+        }
+    }
+
   return (
     <div className='dash-container'>
     <div className='playlist-container'>
@@ -126,9 +157,13 @@ function UnAuthPlaylist() {
         <div className='songs-container'>
           {nItems.map((item)=>{
             return(
-            <div class="track-container"key={item.trackId}>Title: {item.trackTitle} Album: {item.albumTitle} Artist: {item.artistName}
-               <button class="playsong-btn" onClick={() => openInNewTab("https://www.youtube.com/results?search_query="+item.artistName+"-"+item.albumTitle+" "+item.trackTitle)}>Play on Youtube</button> </div>
-            )
+                <div key = { item.trackId } id={item.trackId} className='track-container'>
+                 <div className='default-track' onClick={() => getClicked(item.trackId, item.albumTitle, item.trackDuration)}>
+                    <div>{ item.trackTitle }, Artist: { item.artistName } </div>
+                    <div><button class="playsong-btn" onClick={() => openInNewTab("https://www.youtube.com/results?search_query="+item.artistName+"-"+item.albumTitle+" "+item.trackTitle)}>Play Song</button> </div>
+                </div> 
+                </div>
+               )
           })}
         </div>
       )}
