42b2cd61993efa9e5b0a008b5b68cf6fdac37351
Tara <tlee379@uwo.ca>
2022-11-30 18:31:16 -0500

    more changes

--- Client/src/components/Admin/Admin.js
+++ Client/src/components/Admin/Admin.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link} from "react-router-dom";
 import AdminPublicPlaylistsList from "../AdminPublicPlaylistList/AdminPublicPlaylistList";
 import AdminProfile from "./AdminProfile";
 import "./Admin.css";
